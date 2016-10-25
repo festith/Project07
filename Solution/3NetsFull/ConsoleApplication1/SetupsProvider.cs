ec2ad1464674bf9ca7b540a978a5a0a269af662a
festith <a.festith@gmail.com>
2016-10-25 23:44:19 +0300

    Add networks and setups W2

--- Solution/3NetsFull/ConsoleApplication1/SetupsProvider.cs
+++ Solution/3NetsFull/ConsoleApplication1/SetupsProvider.cs
@@ -5,6 +5,121 @@ namespace ConsoleApplication1
   public static  class SetupsProvider
     {
 
+      public static class SetupsW2
+      {
+          //En
+          public static Setup GetSetupEnW2()
+          {
+              var setup = new Setup();
+              setup.daysForFormPeriod = 30;
+              setup.monthForLevelPeriod = 36;
+              setup.monthForH2H = 24;
+              setup.minValue = 0.03f;
+              setup.maxValue = 0.19f;
+              setup.minKf = 1f;
+              setup.maxKf = 5f;
+              setup.lampda = 0.14f;
+              setup.kf = 1f;
+              return setup;
+          }
+
+          //Fr1 + Fr2
+          public static Setup GetSetupFrW2()
+          {
+              var setup = new Setup();
+              setup.daysForFormPeriod = 30;
+              setup.monthForLevelPeriod = 48;
+              setup.monthForH2H = 36;
+              setup.minValue = 0.03f;
+              setup.maxValue = 0.17f;
+              setup.minKf = 1f;
+              setup.maxKf = 5f;
+              setup.lampda = 0.07f;
+              setup.kf = 1f;
+              return setup;
+          }
+
+          //Ger
+          public static Setup GetSetupGerW2()
+          {
+              var setup = new Setup();
+              setup.daysForFormPeriod = 90;
+              setup.monthForLevelPeriod = 36;
+              setup.monthForH2H = 36;
+              setup.minValue = 0.01f;
+              setup.maxValue = 0.19f;
+              setup.minKf = 1f;
+              setup.maxKf = 5f;
+              setup.lampda = 0.17f;
+              setup.kf = 1f;
+              return setup;
+          }
+
+          //It + Fr1
+          public static Setup GetSetupItW2()
+          {
+              var setup = new Setup();
+              setup.daysForFormPeriod = 90;
+              setup.monthForLevelPeriod = 36;
+              setup.monthForH2H = 48;
+              setup.minValue = 0.03f;
+              setup.maxValue = 0.13f;
+              setup.minKf = 1f;
+              setup.maxKf = 5f;
+              setup.lampda = 0.17f;
+              setup.kf = 1f;
+              return setup;
+          }
+
+          //Nl
+          public static Setup GetSetupNlW2()
+          {
+              var setup = new Setup();
+              setup.daysForFormPeriod = 60;
+              setup.monthForLevelPeriod = 24;
+              setup.monthForH2H = 48;
+              setup.minValue = 0.01f;
+              setup.maxValue = 0.19f;
+              setup.minKf = 1f;
+              setup.maxKf = 4f;
+              setup.lampda = 0.14f;
+              setup.kf = 1f;
+              return setup;
+          }
+
+          //Por + Sp
+          public static Setup GetSetupPorW2()
+          {
+              var setup = new Setup();
+              setup.daysForFormPeriod = 90;
+              setup.monthForLevelPeriod = 24;
+              setup.monthForH2H = 24;
+              setup.minValue = 0.01f;
+              setup.maxValue = 0.22f;
+              setup.minKf = 1f;
+              setup.maxKf = 5f;
+              setup.lampda = 0.225f;
+              setup.kf = 1f;
+              return setup;
+          }
+
+          //Sp
+          public static Setup GetSetupSpW2()
+          {
+              var setup = new Setup();
+              setup.daysForFormPeriod = 60;
+              setup.monthForLevelPeriod = 24;
+              setup.monthForH2H = 48;
+              setup.minValue = 0.03f;
+              setup.maxValue = 0.19f;
+              setup.minKf = 1f;
+              setup.maxKf = 5f;
+              setup.lampda = 0.08f;
+              setup.kf = 1f;
+              return setup;
+          }
+      }
+
       public static class SetupsW1
       {
           //En
@@ -125,16 +240,17 @@ namespace ConsoleApplication1
         {
             var setup = new Setup();
             setup.daysForFormPeriod = 30;
-            setup.monthForLevelPeriod = 48;
-            setup.monthForH2H = 48;
-            setup.minValue = 0.01f;
-            setup.maxValue = 0.11f;
+            setup.monthForLevelPeriod = 36;
+            setup.monthForH2H = 24;
+            setup.minValue = 0.03f;
+            setup.maxValue = 0.19f;
             setup.minKf = 1f;
-            setup.maxKf = 4f;
-            setup.lampda = 0.11f;
+            setup.maxKf = 5f;
+            setup.lampda = 0.14f;
             setup.kf = 1f;
             return setup;
         }
+           
 
       public static class SetupsX
       {
